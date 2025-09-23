11aa799b82d3c9f2541ac61d384d7faff4edbf9f
Wet Noodle <Kluski42@gmail.com>
2025-09-23 18:35:52 -0400

    Modified for winter drop

--- src/main/java/net/wetnoodle/worldwidevillagers/WorldwideVillagers.java
+++ src/main/java/net/wetnoodle/worldwidevillagers/WorldwideVillagers.java
@@ -5,6 +5,7 @@ import net.minecraft.world.entity.npc.VillagerType;
 import net.minecraft.world.level.biome.Biomes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.minecraft.data.worldgen.WinterDropBiomes;
 
 public class WorldwideVillagers implements ModInitializer {
     public static final String MOD_ID = "worldwidevillagers";
@@ -15,7 +16,7 @@ public class WorldwideVillagers implements ModInitializer {
         VillagerType.BY_BIOME.put(Biomes.BIRCH_FOREST, VillagerType.PLAINS);
         VillagerType.BY_BIOME.put(Biomes.OLD_GROWTH_BIRCH_FOREST, VillagerType.PLAINS);
         VillagerType.BY_BIOME.put(Biomes.DARK_FOREST, VillagerType.PLAINS);
-        VillagerType.BY_BIOME.put(Biomes.PALE_GARDEN, VillagerType.PLAINS);
+        VillagerType.BY_BIOME.put(WinterDropBiomes.PALE_GARDEN, VillagerType.PLAINS);
         VillagerType.BY_BIOME.put(Biomes.FOREST, VillagerType.PLAINS);
         VillagerType.BY_BIOME.put(Biomes.FLOWER_FOREST, VillagerType.PLAINS);
         VillagerType.BY_BIOME.put(Biomes.MEADOW, VillagerType.PLAINS);
